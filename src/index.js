d03e583399f19797b6900bd21b9df638f8cf2d51
Barbora Drachovská <bdrachovska@gmail.com>
2021-04-24 12:45:34 +0200

    navigace se schová

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navigation = () => {
+  const navigationClass = document.querySelector('nav');
+  navigationClass.classList.toggle('nav-closed');
+};
+
+const btnElm = document.querySelector('#nav-btn');
+btnElm.addEventListener('click', navigation);
+
+document
+  .querySelectorAll('nav a')
+  .forEach((element) => element.addEventListener('click', navigation));
